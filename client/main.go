55118ebf2fd3e3e822d27dc1329c97abf4026af3
Manav <trivedimanav2003@gmail.com>
2026-07-19 23:07:08 +1000

    Setup client code

--- client/main.go
+++ client/main.go
@@ -1,1 +1,29 @@
-package client
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 10; i++ {
+		wg.Add(1)
+		go func(requestNum int) {
+			defer wg.Done()
+
+			resp, err := http.Get("http://localhost:8080/")
+			if err != nil {
+				fmt.Printf("Request %d: error - %v\n", requestNum, err)
+				return
+			}
+			defer resp.Body.Close()
+
+			fmt.Printf("Request %d: status %d\n", requestNum, resp.StatusCode)
+		}(i)
+	}
+
+	wg.Wait()
+}
