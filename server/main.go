3735a9e2080adb809b34cf9c6a9c6b4e2b033b61
Manav <trivedimanav2003@gmail.com>
2026-07-21 19:08:33 +1000

    Added comments to the server logic

--- server/main.go
+++ server/main.go
@@ -9,14 +9,14 @@ import (
 )
 
 func main() {
-	bucket := limiter.NewTokenBucket(5, 200*time.Millisecond)
+	bucket := limiter.NewTokenBucket(5, 200*time.Millisecond) // max 5 tokens and refills 1 every 200ms
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if !bucket.Allow() {
+		if !bucket.Allow() { // no tokens left
 			http.Error(w, "Too many requests", http.StatusTooManyRequests)
 			return
 		}
-		w.Write([]byte("Request allowed\n"))
+		w.Write([]byte("Request allowed\n")) // token consumed and continue
 	})
 
 	log.Println("Server listening on :8080")
